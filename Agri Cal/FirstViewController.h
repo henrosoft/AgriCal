f5919acf8fda5d1d3362680f8a7f698913000327
Kevin Lindkvist <kevinlindkvist@Kevins-Mac.local>
2012-03-03 17:50:43 -0800

    Initial Commit

---
+++ Agri Cal/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Agri Cal
+//
+//  Created by Kevin Lindkvist on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Agri CalTests/Agri_CalTests.m
@@ -0,0 +1,32 @@
+//
+//  Agri_CalTests.m
+//  Agri CalTests
+//
+//  Created by Kevin Lindkvist on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Agri_CalTests.h"
+
+@implementation Agri_CalTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Agri CalTests");
+}
+
+@end
